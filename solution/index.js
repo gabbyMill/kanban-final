6791f1db40dac909f3ac3734636438ab9150e9e5
gabbyMill <gabbymillatiner@gmail.com>
2021-10-10 23:12:24 +0300

    Re-ordering of code
    Made code a bit more generic, shortened a few functions by a bit.
    Split code into sections

--- solution/index.js
+++ solution/index.js
@@ -1,17 +1,16 @@
-const sections = document.querySelectorAll('.section')
-const buttons = document.querySelectorAll('button')
+//  --- -> Local Storage <- --- \\ Start // Father/Folder
+
 const uls = document.querySelectorAll('ul')
-const searchBar = document.querySelector('#search')
 
 if (!localStorage.tasks) {
+  // for first opening of app
   localStorage.setItem('tasks', '{"todo": [],"in-progress": [],"done": []}') // makes sure there is always a tasks key in local storage
 }
 
 const parsedTasks = JSON.parse(localStorage.getItem('tasks'))
 
 function paintDomFromLocalStorage(dataObject) {
-  const neaterUlNames = []
-  uls.forEach((ul) => neaterUlNames.push(ul.className.split('-tasks')[0]))
+  const neaterUlNames = [...uls].map((ul) => ul.className.split('-tasks')[0])
   // this leaves nicer names to work with
   for (let key in dataObject) {
     for (let i = 0; i < neaterUlNames.length; i++) {
@@ -28,10 +27,42 @@ function paintDomFromLocalStorage(dataObject) {
 }
 paintDomFromLocalStorage(parsedTasks)
 
+function updateParsedTasksAndSetLocalStorage() {
+  const arrayOfUls = [...uls]
+  for (let i = 0; i < arrayOfUls.length; i++) {
+    let category = arrayOfUls[i].classList[0].split('-tasks')[0]
+    for (let key in parsedTasks) {
+      if (key.split('-').join('') === category.split('-').join('')) {
+        const arrayOfLis = [...arrayOfUls[i].querySelectorAll('li')]
+        parsedTasks[key] = []
+        arrayOfLis.forEach((li) => {
+          parsedTasks[key].push(li.textContent)
+        })
+      }
+    }
+    localStorage.setItem('tasks', JSON.stringify(parsedTasks))
+  }
+}
+
+//  --- -> Local Storage <- --- \\ End // Father/Folder
+
+// --------------- -><- --------------- \\ // --------------- -><- --------------- \\
+
+//  --- -> DOM Manipulation & Event Handling <- --- \\ Start // Father/Folder
+
+function addListenersToLi(element) {
+  element.addEventListener('mouseenter', altHandlerFunction) // makes sure you don't have to refresh
+  element.addEventListener('dblclick', makeEditable)
+  element.addEventListener('dragstart', addDragging)
+  element.addEventListener('dragend', removeDragging)
+}
+
 const liS = document.querySelectorAll('li')
 liS.forEach((li) => {
-  li.addEventListener('mouseenter', altHandlerFunction)
+  addListenersToLi(li) // adds all sort of events
 })
+
+//  --- -> Alt functionality <- --- \\ Start // Child
 
 function altHandlerFunction(e) {
   // make sure this only runs while cursor is on <li>
@@ -47,9 +78,6 @@ function altHandlerFunction(e) {
     const keyPressedObject = {}
     keyPressedObject[altClickEvent.key] = true
     document.addEventListener('keydown', function second(secondClickEvent) {
-      // document.removeEventListener('keydown', second)
-      // works with and without the above line but with it
-      // you can't leave your finger on Alt and keep moving it around. Haven't decided which I like better yet.
       if (
         keyPressedObject['Alt'] &&
         numberOptions.includes(secondClickEvent.key)
@@ -78,6 +106,11 @@ function altHandlerFunction(e) {
   }
 }
 
+//  --- -> Alt functionality <- --- \\ End // Child
+
+//  --- -> Add Task <- --- \\ Start // Child
+
+const buttons = document.querySelectorAll('button')
 buttons.forEach((button) => button.addEventListener('click', inputListener))
 
 function getCategory(e) {
@@ -87,6 +120,7 @@ function getCategory(e) {
 }
 
 function inputListener(e) {
+  const searchBar = document.querySelector('#search')
   const clsLst = [...e.target.classList]
   if (
     e.type !== 'keydown' &&
@@ -108,10 +142,7 @@ function inputListener(e) {
   } else {
     newLiElement.className = 'list-item task'
   }
-  newLiElement.addEventListener('mouseenter', altHandlerFunction) // makes sure you don't have to refresh
-  newLiElement.addEventListener('dblclick', makeEditable)
-  newLiElement.addEventListener('dragstart', addDragging)
-  newLiElement.addEventListener('dragend', removeDragging)
+  addListenersToLi(newLiElement)
   if ([...e.target.classList].includes('bold')) {
     newLiElement.classList.add('bold-font') // make bold
   }
@@ -123,6 +154,10 @@ function inputListener(e) {
   input.value = ''
 }
 
+//  --- -> Add Task <- --- \\ End ? // Child
+
+//  --- -> Make Editable <- --- \\ Start // Child
+
 function createLiElement(text, classes = []) {
   const element = document.createElement('li')
   element.append(text)
@@ -131,7 +166,6 @@ function createLiElement(text, classes = []) {
   return element
 }
 
-liS.forEach((li) => li.addEventListener('dblclick', makeEditable))
 function makeEditable(e) {
   e.target.addEventListener('blur', setEditedText)
   e.target.setAttribute('contenteditable', 'true')
@@ -151,6 +185,10 @@ function makeEditable(e) {
   }
 }
 
+//  --- -> Make Editable <- --- \\ End // Child
+
+//  --- -> Search <- --- \\ Start
+
 document.querySelector('#search').addEventListener('input', globalSearch)
 
 function globalSearch(e) {
@@ -168,69 +206,9 @@ function globalSearch(e) {
   })
 }
 
-function createLoader() {
-  const container = document.querySelector('#container')
-  const loader = document.createElement('img')
-  loader.className = 'loader'
-  loader.src = '../gifs/loader.gif'
-  loader.alt = 'image of loader'
-  document.body.insertBefore(loader, container)
-  return loader
-}
+//  --- -> Search <- --- \\ End // Child
 
-document.querySelector('#load-btn').addEventListener('click', loadFromApi)
-document.querySelector('#save-btn').addEventListener('click', saveToApi)
-
-async function loadFromApi() {
-  try {
-    const loader = createLoader()
-    const url = 'https://json-bins.herokuapp.com/bin/614afda34021ac0e6c080cc9'
-    const response = await fetch(url, {
-      method: 'Get',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-    // handle errors:
-    if (!response.ok) {
-      alert(response.status)
-      throw `HTTP Error ${response.status}`
-    }
-    const data = await response.json()
-    if (localStorage.tasks === data.tasks) {
-      return loader.remove()
-    } else {
-      localStorage.setItem('tasks', JSON.stringify(data.tasks))
-    }
-    loader.remove()
-    location.reload()
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-async function saveToApi() {
-  try {
-    const { tasks } = localStorage
-    const loader = createLoader()
-    const url = 'https://json-bins.herokuapp.com/bin/614afda34021ac0e6c080cc9'
-    const response = await fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ tasks: JSON.parse(tasks) }),
-    })
-    if (!response.ok) {
-      alert(response.status)
-      throw `HTTP ERROR ${response.status}`
-    }
-    loader.remove()
-  } catch (e) {
-    console.log(e)
-  }
-}
+//  --- -> D&D <- --- \\ Start
 
 liS.forEach((li) => {
   li.addEventListener('dragstart', () => {
@@ -245,6 +223,7 @@ liS.forEach((li) => {
 })
 
 uls.forEach((ul) => {
+  // notice that you need 'uls' here. get it if it's not in the module with it defined
   ul.addEventListener('dragover', function dragOver(e) {
     e.preventDefault()
     const immediatelyBelowElement = getDragAfterElement(ul, e.clientY) // Gets the element you're immediately above
@@ -286,22 +265,9 @@ function removeDragging(e) {
   e.target.classList.remove('dragging')
 }
 
-function updateParsedTasksAndSetLocalStorage() {
-  const arrayOfUls = [...uls]
-  for (let i = 0; i < arrayOfUls.length; i++) {
-    let category = arrayOfUls[i].classList[0].split('-tasks')[0]
-    for (let key in parsedTasks) {
-      if (key.split('-').join('') === category.split('-').join('')) {
-        const arrayOfLis = [...arrayOfUls[i].querySelectorAll('li')]
-        parsedTasks[key] = []
-        arrayOfLis.forEach((li) => {
-          parsedTasks[key].push(li.textContent)
-        })
-      }
-    }
-    localStorage.setItem('tasks', JSON.stringify(parsedTasks))
-  }
-}
+//  --- -> D&D <- --- \\ End
+
+//  --- -> Color Pallete <- --- \\ Start // Child
 
 let sectionToPaint
 const colorPalettes = document.querySelectorAll('.color')
@@ -309,8 +275,7 @@ colorPalettes.forEach((cp) => {
   cp.addEventListener('click', (e) => {
     const colorPicker = document.querySelector('#picker')
     colorPicker.style = 'display: flex;'
-    const closestSection = e.target.closest('section')
-    sectionToPaint = closestSection
+    sectionToPaint = e.target.closest('section')
     setTimeout(() => {
       colorPicker.style = 'display: none;'
     }, 5000)
@@ -324,10 +289,14 @@ colorPicker.on('color:change', (color) => {
   sectionToPaint.style = `background-color: ${color.hexString}`
 })
 
+//  --- -> Color Pallete <- --- \\ End // Child
+
+//  --- -> Enter Functionality <- --- \\ Start // Child
+
 const inputs = document.querySelectorAll('input')
 inputs.forEach((input) => {
   input.addEventListener('focus', (e) => {
-    if (e.target === searchBar) return
+    if (e.target === document.querySelector('#search')) return
     input.addEventListener('keydown', makeEnterSubmit)
   })
 })
@@ -338,6 +307,10 @@ function makeEnterSubmit(keydownEvent) {
   }
 }
 
+//  --- -> Enter Functionality <- --- \\ End // Child
+
+//  --- -> Bold & Underline <- --- \\ Start // Child
+
 const boldIcons = document.querySelectorAll('.bold')
 boldIcons.forEach((icon) => {
   icon.addEventListener('click', inputListener)
@@ -347,3 +320,90 @@ const underlinedIcons = document.querySelectorAll('.underline')
 underlinedIcons.forEach((icon) => {
   icon.addEventListener('click', inputListener)
 })
+
+//  --- -> Bold & Underline <- --- \\ End // Child
+
+//  --- -> DOM Manipulation & Event Handling <- --- \\ End // Father/Folder
+
+// --------------- -><- --------------- \\ // --------------- -><- --------------- \\
+
+//  --- -> Network <- --- \\ Start // Father/Folder
+
+//  --- -> createLoader <- --- \\ Start // Child
+
+function createLoader() {
+  const container = document.querySelector('#container')
+  const loader = document.createElement('img')
+  loader.className = 'loader'
+  loader.src = '../gifs/loader.gif'
+  loader.alt = 'image of loader'
+  document.body.insertBefore(loader, container)
+  return loader
+}
+
+//  --- -> Create Loader <- --- \\ End // Child
+
+document.querySelector('#load-btn').addEventListener('click', loadFromApi)
+document.querySelector('#save-btn').addEventListener('click', saveToApi)
+
+//  --- -> Load From Api <- --- \\ Start // Child
+
+async function loadFromApi() {
+  try {
+    const loader = createLoader()
+    const url = 'https://json-bins.herokuapp.com/bin/614afda34021ac0e6c080cc9'
+    const response = await fetch(url, {
+      method: 'Get',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    // handle errors:
+    if (!response.ok) {
+      alert(response.status)
+      throw `HTTP Error ${response.status}`
+    }
+    const data = await response.json()
+    if (localStorage.tasks === data.tasks) {
+      return loader.remove()
+    } else {
+      console.log(localStorage)
+      localStorage.setItem('tasks', JSON.stringify(data.tasks))
+    }
+    loader.remove()
+    location.reload()
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+//  --- -> Load From Api <- --- \\ End // Child
+
+//  --- -> Save To Api <- --- \\ Start // Child
+
+async function saveToApi() {
+  try {
+    const { tasks } = localStorage
+    const loader = createLoader()
+    const url = 'https://json-bins.herokuapp.com/bin/614afda34021ac0e6c080cc9'
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ tasks: JSON.parse(tasks) }),
+    })
+    if (!response.ok) {
+      alert(response.status)
+      throw `HTTP ERROR ${response.status}`
+    }
+    loader.remove()
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+//  --- -> Save To Api <- --- \\ End // Child
+
+//  --- -> Network <- --- \\ End // Father/Folder
